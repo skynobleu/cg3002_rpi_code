93c0b19ab48ae0ffbce2452ee8c7eceffa09c48f
Wei Jie <ngweijie94@gmail.com>
2017-10-24 15:34:35 +0800

    changed pi env settings

--- install_env.sh
+++ install_env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-conda create -n pi_env python=3.4.3 scipy=0.16.0 pandas=0.16.2 numpy=1.9.2 scikit-learn=0.16.1
+conda create -n pi_env python=3.4.3 scipy=0.16.0 pandas=0.16.2 numpy=1.9.2 scikit-learn=0.16.1 pyserial
