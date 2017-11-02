8dd15c7500010fc398296c5a2e4f6c6db5f42b3d
Wei Jie <ngweijie94@gmail.com>
2017-11-02 15:58:15 +0800

    made changes to comm and main for cumulative power

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ import time
 classifier = Software("bin/models/KNN_new.sav")
 
 s = comm(serial.Serial("/dev/ttyS0",115200))
-#c = client("192.168.1.233", 8888)
+c = client("192.168.1.233", 8888)
 s.handshake()
 previous = "started"
 reset = False
+prevTime = 0
+cumpower = 0
+
 while True:
+    currTime = time.time()
     dataList = s.receive()
-    preData = dataList[4]
+    preData = dataList[3]
     #predicting function here
     actnum = classifier.predictDanceMove(preData)
 
@@ -22,11 +26,11 @@ while True:
         current = dataList[0]
         voltage = dataList[1]
         inspower = "{:.3f}".format(dataList[2])
-        cumpower = "{:.3f}".format(dataList[3])
+        cumpower += "{:.3f}".format((currTime - prevTime) * inspower)
         if previous != "started":
             if previous == actnum:
                 print("send: " + actnum)
-                #c.clientsend(actnum,voltage,current,inspower,cumpower)
+                c.clientsend(actnum,voltage,current,inspower,cumpower)
                 previous = "resetted"
                 reset = True
         #else:
@@ -35,3 +39,5 @@ while True:
         previous = actnum
     else:
         reset = False
+
+    prevTime = currTime
