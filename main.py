d7a82b7e017e0beb2715a50897348dca438ab61e
rasp-pi <ngweijie94@gmail.com>
2017-11-15 09:56:18 +0000

    first final prediction of pi

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import time
 classifier = Software("bin/models/KNN_new.sav")
 
 s = comm(serial.Serial("/dev/ttyS0",115200))
-c = client("192.168.1.233", 8888)
+c = client("192.168.1.20", 8800)
 s.handshake()
 previous = "started"
 reset = False
 prevTime = 0
-cumpower = 0
+cumpower = 0.0
 
 while True:
     currTime = time.time()
@@ -26,11 +26,11 @@ while True:
         current = dataList[0]
         voltage = dataList[1]
         inspower = "{:.3f}".format(dataList[2])
-        cumpower += "{:.3f}".format((currTime - prevTime) * inspower)
+        cumpower += float("{:.3f}".format(float(currTime - prevTime) * float(inspower)))
         if previous != "started":
             if previous == actnum:
                 print("send: " + actnum)
-                c.clientsend(actnum,voltage,current,inspower,cumpower)
+                c.clientsend(actnum,voltage,current,inspower,str(cumpower))
                 previous = "resetted"
                 reset = True
         #else:
